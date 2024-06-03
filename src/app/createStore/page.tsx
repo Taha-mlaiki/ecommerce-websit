3cadef371847efe69435e6b4db949845cdd2d206
taha <mlaikitaha29@gmail.com>
2024-06-03 15:12:28 +0100

    first commit

--- src/app/createStore/page.tsx
+++ src/app/createStore/page.tsx
@@ -3,14 +3,17 @@ import { useStoreModal } from "@/hooks/use-store-model";
 import React, { useEffect } from "react";
 import ModalProvider from '@/providers/ModalProvider'
 import "../globals.css"
+
 const page = () => {
   const isOpen = useStoreModal((state)=> state.isOpen)
   const onOpen = useStoreModal((state)=> state.onOpen)
+
   useEffect(()=>{
     if(!isOpen){
       onOpen();
     }
   },[isOpen,onOpen])
+  
   return (
     <div>
       <ModalProvider />
